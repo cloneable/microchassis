319a4b9e10542df80f2908f3f26090d41b4f7251
Folke Behrens <folke@gmail.com>
2023-02-19 19:29:48 +0100

    Add build.rs and some package info

---
+++ microchassis/build.rs
@@ -0,0 +1,24 @@
+use std::env;
+
+const ENV_PROFILE: &str = "MICROCHASSIS_BUILD_PROFILE";
+const ENV_MICROCHASSIS_BUILD_PROFILE: &str = "MICROCHASSIS_BUILD_PROFILE";
+
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-env-changed={ENV_PROFILE}");
+    println!("cargo:rerun-if-env-changed={ENV_MICROCHASSIS_BUILD_PROFILE}");
+
+    let cargo_profile = env::var(ENV_PROFILE).ok();
+    let microchassis_build_profile = env::var(ENV_MICROCHASSIS_BUILD_PROFILE).ok();
+
+    let profile = match microchassis_build_profile.as_deref() {
+        Some(profile) => profile,
+        None => match cargo_profile.as_deref() {
+            Some("release") => "release",
+            _ => "debug",
+        },
+    };
+
+    println!("cargo:rustc-env={ENV_MICROCHASSIS_BUILD_PROFILE}={profile}");
+
+    Ok(())
+}
